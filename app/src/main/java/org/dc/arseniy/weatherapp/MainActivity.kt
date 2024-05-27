20fde2fb8fc3b77b2a84289b1bb4e5b510de8992
Arseniy <oav22mk309@student.bmstu.ru>
2024-05-27 14:23:21 +0300

    first

---
+++ app/src/main/java/org/dc/arseniy/weatherapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package org.dc.arseniy.weatherapp
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.dc.arseniy.weatherapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textField.setText("fffff")
+    }
+}
