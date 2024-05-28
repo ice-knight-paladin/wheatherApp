90357f79fd9da65919eb4c7e1ad252824eef37fd
Arseniy <oav22mk309@student.bmstu.ru>
2024-05-29 00:53:13 +0300

    try2

--- app/src/main/java/org/dc/arseniy/weatherapp/MainActivity.kt
+++ app/src/main/java/org/dc/arseniy/weatherapp/MainActivity.kt
@@ -2,21 +2,31 @@ package org.dc.arseniy.weatherapp
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.View
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import org.dc.arseniy.weatherapp.databinding.ActivityMainBinding
 import java.util.Date
+import java.util.Locale
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,8 +39,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var latitude :String
-    private lateinit var longitude :String
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,20 +48,105 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         requestPermissions(INITIAL_PERMS, INITIAL_REQUEST)
-        binding.btnLocationSettings.setOnClickListener { object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
-            }
-            @SuppressLint("MissingPermission")
-            private fun obtieneLocalizacion(){
-                fusedLocationClient.lastLocation
-                    .addOnSuccessListener { location: Location? ->
-                        latitude =  location?.latitude.toString()
-                        longitude = location?.longitude.toString()
+        checkLocation()
+
+    }
+    private fun checkLocation(){
+        val manager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            showAlertLocation()
+        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        getLocationUpdates()
+    }
+    private fun showAlertLocation() {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setMessage("Your location settings is set to Off, Please enable location to use this application")
+        dialog.setPositiveButton("Settings") { _, _ ->
+            val myIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(myIntent)
+        }
+        dialog.setNegativeButton("Cancel") { _, _ ->
+            finish()
+        }
+        dialog.setCancelable(false)
+        dialog.show()
+    }
+    private fun getLocationUpdates() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest()
+        locationRequest.interval = 50000
+        locationRequest.fastestInterval = 50000
+        locationRequest.smallestDisplacement = 170f //170 m = 0.1 mile
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY //according to your app
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                if (locationResult.locations.isNotEmpty()) {
+                    /*val location = locationResult.lastLocation
+                    Log.e("location", location.toString())*/
+                    val addresses: List<Address>?
+                    val geoCoder = Geocoder(applicationContext, Locale.getDefault())
+                    addresses = geoCoder.getFromLocation(
+                        locationResult.lastLocation.latitude,
+                        locationResult.lastLocation.longitude,
+                        1
+                    )
+                    if (addresses != null && addresses.isNotEmpty()) {
+                        val address: String = addresses[0].getAddressLine(0)
+                        val city: String = addresses[0].locality
+                        val state: String = addresses[0].adminArea
+                        val country: String = addresses[0].countryName
+                        val postalCode: String = addresses[0].postalCode
+                        val knownName: String = addresses[0].featureName
+                        binding.tvLocationGPS.text = "$address $city $state $postalCode $country $knownName"
+                        Log.e("location", "$address $city $state $postalCode $country $knownName")
                     }
+                }
             }
-        } }
-        binding.tvLocationGPS.text = latitude+" " + longitude
+        }
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null /* Looper */
+        )
+    }
+
+    // Stop location updates
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    // Stop receiving location update when activity not visible/foreground
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    // Start receiving location update when activity  visible/foreground
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
     }
 
 }
