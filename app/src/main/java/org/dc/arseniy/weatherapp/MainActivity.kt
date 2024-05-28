0370a8e2114e5ba023c89463f90041f43a0c35de
Arseniy <oav22mk309@student.bmstu.ru>
2024-05-29 00:36:34 +0300

    try1

--- app/src/main/java/org/dc/arseniy/weatherapp/MainActivity.kt
+++ app/src/main/java/org/dc/arseniy/weatherapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package org.dc.arseniy.weatherapp
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
@@ -12,6 +13,8 @@ import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import org.dc.arseniy.weatherapp.databinding.ActivityMainBinding
 import java.util.Date
 
@@ -25,48 +28,30 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var locationManager: LocationManager? = null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var latitude :String
+    private lateinit var longitude :String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         requestPermissions(INITIAL_PERMS, INITIAL_REQUEST)
-
-        binding.btnLocationSettings.setOnClickListener(object : View.OnClickListener{
-            @RequiresApi(Build.VERSION_CODES.P)
+        binding.btnLocationSettings.setOnClickListener { object : View.OnClickListener{
             override fun onClick(v: View?) {
-                locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
-                if (ActivityCompat.checkSelfPermission(
-                        this@MainActivity,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        this@MainActivity,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return
-                }
-                binding.tvTitleGPS.text =
-                    locationManager?.getLastKnownLocation(LocationManager.GPS_PROVIDER)?.latitude.toString() + " " +
-                            locationManager?.getLastKnownLocation(LocationManager.GPS_PROVIDER)?.longitude.toString()
-
-                binding.tvTitleNet.text =
-                    locationManager?.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)?.latitude.toString() + " " +
-                        locationManager?.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)?.longitude.toString()
-                binding.tvEnabledGPS.text = locationManager?.isProviderEnabled(LocationManager.GPS_PROVIDER).toString()
-                binding.tvEnabledNet.text = locationManager?.isProviderEnabled(LocationManager.NETWORK_PROVIDER).toString()
-                System.out.println(String.equals(LocationManager.NETWORK_PROVIDER))
+                fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
             }
-        })
+            @SuppressLint("MissingPermission")
+            private fun obtieneLocalizacion(){
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        latitude =  location?.latitude.toString()
+                        longitude = location?.longitude.toString()
+                    }
+            }
+        } }
+        binding.tvLocationGPS.text = latitude+" " + longitude
     }
 
 }
